984133b81e9f62a14b1b9a49d42bc192b54819d5
Ayan Ansari <mayan9917@gmail.com>
2024-02-24 11:59:16 +0530

    add a folder and change the file's name

---
+++ ImageGallery/script.js
@@ -0,0 +1,15 @@
+const scrollContainer = document.querySelector(".gallery");
+const backBtn = document.getElementById("backBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+backBtn.addEventListener("click", () => {
+  scrollContainer.scrollLeft -= 900;
+});
